1193d0d1b3e66fa7b2398ca10808e35b588a559e
PSamu23 <samuelespinal90@gmail.com>
2025-04-04 20:19:58 -0600

    feature/samuel/add_entitis

---
+++ Backend/Domain/Entities/Ciudad.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using Domain.Common
+
+namespace Domain.Entities
+{
+    public class Ciudad: EntityCatalog
+    {
+
+        public int DepartamentoId { get; set; }
+
+        public Departamento? Departamento { get; set; }
+
+        public Ciudad(string Descripcion) {
+
+            this.Descripcion = Descripcion;
+        
+        }
+    }
+}
---
+++ Backend/Domain/Entities/Modelo.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Text.RegularExpressions;
+using System.Threading.Tasks;
+using Domain.Common;
+
+namespace Domain.Entities
+{
+    public class Modelo:EntityCatalog
+    {
+        public int IdMarca { get; set; }
+
+        public Marca? Marca { get; set; }
+    }
+
+}
