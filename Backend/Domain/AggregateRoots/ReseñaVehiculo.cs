54f2484c8f60a1daff4aed75f281e4ce53d9d917
PSamu23 <samuelespinal90@gmail.com>
2025-04-04 20:42:54 -0600

    featura/samuel/20250404/add_class_resenia_vehiculo

---
+++ Backend/Domain/AggregateRoots/ReseñaVehiculo.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using Domain.Entities;
+using Domain.Common;
+
+namespace Domain.AggregateRoots
+{
+    public class ReseñaVehiculo:AggregateRoot
+    {
+        public int IdUsuarioHuesped { get; set; }
+        public int IdVehiculo { get; set; }
+        public DateTime FechaReseña { get; set; }
+        public string Comentario { get; set; }
+        public int IdValoracion { get; set; }
+
+        // Navegación
+        public Usuario? UsuarioHuesped { get; set; }
+        public Vehiculo? Vehiculo { get; set; }
+        public Valoracion? Valoracion { get; set; }
+    }
+}
