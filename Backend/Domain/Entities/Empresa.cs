eb90bff91f78c80fac046c5670bebefde8a219b0
susandv26 <valladares.susan@yahoo.com>
2025-04-04 21:24:02 -0600

    Feature:Addition of new entities: WorkArea, User, PaymentMethod, Company

---
+++ Backend/Domain/Entities/Empresa.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using Domain.Common;
+
+namespace Domain.Entities
+{
+    public class Empresa:Entity
+    {
+        public string? Nombre { get; set; }
+        public string? Rtn { get; set; }
+        public string? Correo { get; set; }
+        public string? CasaMatriz { get; set; }
+        public string? Telefono { get; set; }
+
+        public List<Sucursal> Sucursal { get; set; } = new();
+    }
+}
---
+++ Backend/Domain/Entities/Usuario.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using Domain.Common;
+
+namespace Domain.Entities
+{
+    public class Usuario:Entity
+    {
+        public string? Email { get; set; }
+        public string? Password { get; set; }
+        public string? Dni { get; set; }
+        public int RolId { get; set; }
+        public Persona? Persona { get; set; }
+        public Rol? Rol { get; set; }
+
+    }
+}
