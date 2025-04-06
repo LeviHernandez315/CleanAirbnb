c3b5068d3669f37a4cacb5e4f5ee233921b8db2f
susandv26 <valladares.susan@yahoo.com>
2025-04-06 15:52:15 -0600

    Added complete structure of MetodoPago (DTOs, interfaces, service and repository)

---
+++ Backend/API/Controllers/EntitiesControllers/MetodoPagoController.cs
@@ -0,0 +1,63 @@
+﻿using Application.DTOs.Entidades.MetodoPagoDTOs;
+using Application.Interfaces.Entidades.MetodoPagoInterfaces;
+using Microsoft.AspNetCore.Mvc;
+
+namespace API.Controllers.EntitiesControllers
+{
+    [ApiController]
+    [Route("api/MetodoPagos")]
+    public class MetodoPagoController : ControllerBase
+    {
+        private readonly IMetodoPagoService _metodoPagoService;
+
+        public MetodoPagoController(IMetodoPagoService metodoPagoService)
+        {
+            _metodoPagoService = metodoPagoService;
+        }
+
+  
+        [HttpGet]
+        public async Task<IActionResult> GetAll()
+        {
+            var lista = await _metodoPagoService.GetAllAsync();
+            return Ok(lista); 
+        }
+
+        [HttpGet("{id}")]
+        public async Task<IActionResult> GetById(int id)
+        {
+            var metodo = await _metodoPagoService.GetByIdAsync(id);
+            return Ok(metodo); 
+        }
+
+      
+        [HttpPost]
+        public async Task<IActionResult> Create([FromBody] MetodoPagoRequestDTO dto)
+        {
+            var metodoPago = await _metodoPagoService.CreateAsync(dto);
+            return CreatedAtAction(nameof(GetById), new { id = metodoPago.Id },metodoPago); 
+        }
+
+       
+        [HttpPut("{id}")]
+        public async Task<IActionResult> Update(int id, [FromBody] MetodoPagoRequestDTO dto)
+        {
+            var update = await _metodoPagoService.UpdateAsync(id, dto);
+            if (!update)
+                return NotFound(); 
+
+            return NoContent(); 
+        }
+
+      
+        [HttpDelete("{id}")]
+        public async Task<IActionResult> Delete(int id)
+        {
+            var delete = await _metodoPagoService.DeleteAsync(id);
+            if (!delete)
+                return NotFound(); 
+
+            return NoContent(); 
+        }
+    }
+}
---
+++ Backend/Application/Interfaces/Entidades/MetodoPagoInterfaces/IMetodoPagoService.cs
@@ -0,0 +1,14 @@
+﻿
+using Application.DTOs.Entidades.MetodoPagoDTOs;
+
+namespace Application.Interfaces.Entidades.MetodoPagoInterfaces
+{
+    public interface IMetodoPagoService
+    {
+        Task<IEnumerable<MetodoPagoResponseDTO>> GetAllAsync();
+        Task<MetodoPagoResponseDTO> GetByIdAsync(int id);
+        Task<MetodoPagoResponseDTO> CreateAsync(MetodoPagoRequestDTO dto);
+        Task<bool> UpdateAsync(int id, MetodoPagoRequestDTO dto);
+        Task<bool> DeleteAsync(int id);
+    }
+}
