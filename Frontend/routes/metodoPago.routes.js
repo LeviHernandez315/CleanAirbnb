52087f6f8ccd3836f03f573934e8fa91a699e93c
PSamu23 <samuelespinal90@gmail.com>
2025-04-06 14:34:21 -0600

    hotfix: delete file we dont need, becouse it gives some issius

--- Frontend/routes/metodoPago.routes.js
+++
@@ -1,9 +0,0 @@
-var express = require('express');
-var router = express.Router();
-
-var meotodoPagoController = require('../controllers/metodoPago.controllers')
-
-router.get('/', meotodoPagoController.obtenerMetodoPago);
-
-
-module.exports = router;
--- Frontend/routes/personas.routes.js
+++
@@ -1,9 +0,0 @@
-var express = require('express');
-var router = express.Router();
-var personasController = require('../controllers/personas.controllers')
-
-router.get('/', personasController.obtenerUsuarios);
-router.post('/', personasController.insertarPersona);
-router.post('/usuarios', personasController.insertarUsuario);
-
-module.exports = router;
